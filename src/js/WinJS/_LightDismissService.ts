c24513c6046098aeebb2c7738607712e60d25276
TimTim <timothywong@codefreesoft.com>
2018-10-09 11:01:37 +0800

    Increase base z-index for light dismissables

--- src/js/WinJS/_LightDismissService.ts
+++ src/js/WinJS/_LightDismissService.ts
@@ -115,7 +115,7 @@ require(["require-style!less/styles-lightdismissservice"]);
 //       - Tries to give focus to the root element of the dismissable.
 //
 
-var baseZIndex = 1000;
+var baseZIndex = 10000;
 
 var Strings = {
     get closeOverlay() { return _Resources._getWinJSString("ui/closeOverlay").value; }
